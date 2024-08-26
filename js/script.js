174144eb01b4e85c51b36920d9bbf0bf17b7ac10
Şeyma Çelebi <celebiseyma98@gmail.com>
2024-08-26 23:02:21 +0300

    some script added

--- js/script.js
+++ js/script.js
@@ -14,3 +14,29 @@ document.addEventListener('DOMContentLoaded', function() {
     // calculateAge fonksiyonunu buton click eventine bağlayın
     document.querySelector('button').onclick = calculateAge;
 });
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    section.forEach( sec=> {
+        let top =window.scrollY;
+        let offset = sec.offsetTop -100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            //active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        }
+    });
+
+    //sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
